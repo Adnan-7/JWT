02b0846bd3956f1dea19dfb60250f6d2569ed930
Adnan Khan <adnan7.zain@gmail.com>
2022-05-11 23:47:33 +0500

    sign jwt

--- controllers/main.js
+++ controllers/main.js
@@ -1,5 +1,32 @@
+// check username, password in post(login) request
+// if exist create new JWT
+// send back to fron-end
+// setup authentication so only the request with JWT can access the dasboard
+
+const { json } = require('express/lib/response');
+const jwt = require('jsonwebtoken');
+const CustomAPIError = require('../errors/custom-error');
+
 const login = async (req, res) => {
-  res.send('Fake Login/Register/Singup Route');
+  const { password, username } = req.body;
+  // mongoose validation
+  // Joi
+  // check in the controller
+
+  if (!username || !password) {
+    throw new CustomAPIError('please provide username and password', 400);
+  }
+
+  //just for demo, normally provided by DB!!!!
+  const id = new Date().getDate();
+
+  // try to keep payload small, better experience for user
+  // just for demo, for JWT.secret in production use long, complex and unguessable string value!!!!!!!!!
+  const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
+    expiresIn: '30d',
+  });
+
+  res.status(200).json({ msg: 'user created', token });
 };
 
 const dashboard = async (req, res) => {
